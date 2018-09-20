f4b09fff7c5e6604c57ef7bca11c60094712264e
ostpshembekar <pshembekar@ostglobal.com>
2018-09-20 12:47:48 -0400

    Started working on delete functionality

---
+++ TechStack/Pages/Catalog/Delete.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TechStack.Infrastructure;
+
+namespace TechStack.Pages.Catalog
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Catalog Catalog { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Catalog = await _context.Catalog.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Catalog == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Catalog = await _context.Catalog.FindAsync(id);
+
+            if (Catalog != null)
+            {
+                _context.Catalog.Remove(Catalog);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+}
